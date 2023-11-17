fbf0be966ddabdb1b4cd06b2ab25fe10f4acb63c
Lukas Köllner <lkoe@bluewin.ch>
2023-11-17 11:42:39 +0000

    Use first command line option as root path

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from IndexTranslations import find_translation_files
 from RailsTranslatorException import *
 from dotenv import load_dotenv
 from CatchUp import catch_up
+import sys
 import os
 
 
 def main():
     load_dotenv()
+    if len(sys.argv) > 1:
+        root_path = sys.argv[1]
+    else:
+        root_path = input("What is the Rails root path? ")
 
-    root_path = input("What is the Rails root path? ")
     locales_path = os.path.join(root_path, "config/locales")
 
     if not os.path.exists(locales_path):
